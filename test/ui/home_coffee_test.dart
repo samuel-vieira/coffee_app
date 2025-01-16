e3d4f40c667d267a77dab3adbd85664db7ac6779
samuel-vieira <samuel.w.s.n@gmail.com>
2025-01-16 00:53:19 -0300

    Adding widget tests to validate the navigation from home screen to details coffee page

--- test/ui/home_coffee_test.dart
+++ test/ui/home_coffee_test.dart
@@ -1,10 +1,10 @@
 import 'package:coffee_app/main.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets("Tap on get start button and navigate to HomeScreen",(WidgetTester tester) async {
-    // Build App.
+  testWidgets("Tap on Card and go to CoffeeDetailsScreen", (WidgetTester tester) async {
+    // Build App
     await tester.pumpWidget(const CoffeeApp());
 
     // Check if you are on the Get Started page.
@@ -14,7 +14,14 @@ void main() {
     await tester.tap(find.byKey(const Key('getStartedButton')));
     await tester.pumpAndSettle();
 
-    // Check if you are on the right page.
+    // Check if the app is on the right page.
     expect(find.byKey(const Key("homeScreen")), findsOneWidget);
+
+    // Tap on Card to go to details.
+    await tester.tap(find.byKey(const Key('coffeeCard-0')));
+    await tester.pumpAndSettle();
+
+    // Check if the app is on the right page.
+    expect(find.byKey(const Key("coffeeDetailsScreen")), findsOneWidget);
   });
 }
